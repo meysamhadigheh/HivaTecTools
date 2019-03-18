9e3d68adcaa567db4372145d5441583372287916
ashkan <ashkan@gmail.com>
2019-03-18 17:37:59 +0330

    added hivabutton width and visibility

--- hivatectools/src/main/java/hivatec/ir/hivatectools/helper/PhoneInformation.java
+++ hivatectools/src/main/java/hivatec/ir/hivatectools/helper/PhoneInformation.java
@@ -8,6 +8,8 @@ import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 
+import java.util.UUID;
+
 /**
  * Created by Saeed on 6/2/2015.
  */
@@ -26,30 +28,12 @@ public class PhoneInformation {
      * @return imei
      */
     public static String IMEI(Context context) {
-        try {
 
-            String identifier = null;
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            if (tm != null)
-                if (ActivityCompat.checkSelfPermission(G.context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return "token";
-                }identifier = tm.getDeviceId();
-            if (identifier == null || identifier.length() == 0)
-                identifier = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-
-            return identifier;
-
-        } catch (Exception e) {
-            String dd = e.getMessage();
-            return "token";
+        if (!SharedPreference.contains("imei")) {
+            SharedPreference.putString("imei", randomIMEI());
         }
+
+        return SharedPreference.getString("imei", randomIMEI());
     }
 
     /**
@@ -59,18 +43,7 @@ public class PhoneInformation {
      */
     public static String PhoneNumber(Context context) {
         try {
-            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            if (ActivityCompat.checkSelfPermission(G.context, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(G.context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return "token";
-            }
-            return telephonyManager.getLine1Number();
+            return "not found";
         } catch (Exception e) {
             return "";
         }
@@ -91,4 +64,11 @@ public class PhoneInformation {
         int sdkVersion = Build.VERSION.SDK_INT;
         return sdkVersion + " (" + release +")";
     }
+
+    public static String randomIMEI() {
+
+        return UUID.randomUUID().toString();
+    }
+
+
 }
